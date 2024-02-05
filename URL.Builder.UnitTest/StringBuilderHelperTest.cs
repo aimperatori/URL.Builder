4eb55525f44ef434206f3cc7e84fb612b0e46d31
aimperatori <aimperatori@ucs.br>
2024-02-05 20:15:51 -0300

    created test to StringBuilderHelper

---
+++ URL.Builder.UnitTest/StringBuilderHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using URL.Builder.Helper;
+
+namespace URL.Builder.UnitTest
+{
+    [TestClass]
+    public class StringBuilderHelperTest
+    {
+        [TestMethod]
+        public void TrimEmptyStringTest()
+        {
+            var output = "";
+
+            var stringBuilder = StringBuilderHelper.TrimEnd(new StringBuilder(""), '/');
+
+            Assert.AreEqual(output, stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TrimACharTest()
+        {
+            var output = "aa";
+
+            var stringBuilder = StringBuilderHelper.TrimEnd(new StringBuilder("aa//"), '/');
+
+            Assert.AreEqual(output, stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TrimNullStringBuilderTest()
+        {
+            var stringBuilder = StringBuilderHelper.TrimEnd(null, '/');
+
+            Assert.AreEqual(null, stringBuilder);
+        }
+    }
+}
